5b50fca50add144226e73e5c90a5bb8095e37dcb
Wolf Mermelstein <wolfmermelstein@gmail.com>
2024-06-30 19:39:15 -0400

    Fix paths

--- flake.nix
+++ flake.nix
@@ -23,8 +23,10 @@
             postFixup = ''
               ${previousAttrs.postFixup or ""}
               wrapProgram $out/bin/nvim \
+                --set PYTHONPATH $PYTHONPATH:${pkgs.python312Packages.std2}/lib:${pkgs.python312Packages.pynvim-pp}/lib:${pkgs.python312Packages.pyyaml}/lib \
                 --set PATH $PATH:${
                   pkgs.lib.makeBinPath [
+                    pkgs.nodejs_22
                     pkgs.git
                     pkgs.nixd
                     pkgs.nodePackages.prettier
@@ -49,8 +51,37 @@
                     pkgs.ripgrep
                     pkgs.fd
                     pkgs.rustfmt
-                    pkgs.tree-sitter
                     pkgs.pandoc
+                    pkgs.tree-sitter
+                    pkgs.tree-sitter-grammars.tree-sitter-typst
+                    pkgs.tree-sitter-grammars.tree-sitter-latex
+                    pkgs.tree-sitter-grammars.tree-sitter-yaml
+                    pkgs.tree-sitter-grammars.tree-sitter-vim
+                    pkgs.tree-sitter-grammars.tree-sitter-typescript
+                    pkgs.tree-sitter-grammars.tree-sitter-toml
+                    pkgs.tree-sitter-grammars.tree-sitter-sql
+                    pkgs.tree-sitter-grammars.tree-sitter-scss
+                    pkgs.tree-sitter-grammars.tree-sitter-rust
+                    pkgs.tree-sitter-grammars.tree-sitter-r
+                    pkgs.tree-sitter-grammars.tree-sitter-regex
+                    pkgs.tree-sitter-grammars.tree-sitter-python
+                    pkgs.tree-sitter-grammars.tree-sitter-prisma
+                    pkgs.tree-sitter-grammars.tree-sitter-php
+                    pkgs.tree-sitter-grammars.tree-sitter-perl
+                    pkgs.tree-sitter-grammars.tree-sitter-nix
+                    pkgs.tree-sitter-grammars.tree-sitter-markdown
+                    pkgs.tree-sitter-grammars.tree-sitter-markdown-inline
+                    pkgs.tree-sitter-grammars.tree-sitter-lua
+                    pkgs.tree-sitter-grammars.tree-sitter-julia
+                    pkgs.tree-sitter-grammars.tree-sitter-json
+                    pkgs.tree-sitter-grammars.tree-sitter-javascript
+                    pkgs.tree-sitter-grammars.tree-sitter-java
+                    pkgs.tree-sitter-grammars.tree-sitter-html
+                    pkgs.tree-sitter-grammars.tree-sitter-haskell
+                    pkgs.tree-sitter-grammars.tree-sitter-graphql
+                    pkgs.tree-sitter-grammars.tree-sitter-go
+                    pkgs.tree-sitter-grammars.tree-sitter-fish
+                    pkgs.tree-sitter-grammars.tree-sitter-dockerfile
                   ]
                 }
             '';
